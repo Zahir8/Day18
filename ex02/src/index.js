662556b9f7a20b223d84951dfdbd270fec8cbdda
zahirbegulic@gmail.com <zahirbegulic@gmail.com>
2021-03-25 20:49:39 +0100

    Delegating to another generator

---
+++ ex02/src/index.js
@@ -0,0 +1,35 @@
+function* myGenerator(){
+    yield* insideGenerator1();
+    yield* insideGenerator2();
+    yield* insideGenerator3();
+};
+function* insideGenerator1(){
+    for(let i = 1; i <= 5; i++){
+        let x = i;
+        yield x;
+    }
+};
+function* insideGenerator2(){
+    for(let i = 10; i <= 15; i++){
+        let x = i;
+        yield x;
+    }
+};
+function* insideGenerator3(){
+    for(let i = 6; i <= 9; i++){
+        let x = i;
+        yield x;
+    }
+};
+
+let iterator = myGenerator();
+
+let fifteenArray = [];
+
+for(let e of iterator){
+    fifteenArray.push(e);
+    fifteenArray.push(iterator.next(e).value);
+
+}
+console.log(fifteenArray);
+module.exports = {fifteenArray, myGenerator};
